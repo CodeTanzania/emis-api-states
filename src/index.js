f5c83fe6714ef52f76cfad934abffafe008f469c
Benson Maruchu <benmaruchu@gmail.com>
2019-01-21 10:01:15 +0300

    docs(index): add usage example for StoreProvider

--- src/index.js
+++ src/index.js
@@ -18,6 +18,11 @@ import { store } from './store';
  * @returns {ReactComponent} Provider
  * @version 0.1.0
  * @since 0.1.0
+ * @example
+ * import {StoreProvider} from '@codetanzania/emis-api-states';
+ *
+ * ReactDom.render(<StoreProvider><App /></StoreProvider>,
+ * document.getElementById('root'));
  */
 export function StoreProvider({ children }) {
   return <Provider store={store}>{children}</Provider>;
